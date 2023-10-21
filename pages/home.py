f9e266f6717febe7942dfad99cd8ea486ee6a339
samurai-py <marioestevao@gmail.com>
2023-10-21 10:13:26 -0300

    Organizando branches

--- pages/home.py
+++ pages/home.py
@@ -8,5 +8,5 @@ dash.register_page(__name__, path='/')
 layout = html.Div([
     html.H3("Página Inicial", style={'color': 'white'}),
     # Adicione o conteúdo específico para esta página
-    html.P("Seja bem-vindo ao Cérebro Político. Somos uma base de dados aberta sobre informações importantes a respeito de tomada de decisão democrática. Qualquer pessoa pode colaborar com o projeto, e aqui voce podera encontrar informações sobre pessoas que exercem cargos públicos, gastos governamentais, além de diversas métricas de âmbito social, econômico, tecnológico e demográfico.")
+    html.P("Seja bem-vindo ao Cérebro Político. Somos uma base de dados aberta sobre informações importantes a respeito de tomada de decisão democrática. Qualquer pessoa pode colaborar com o projeto, e aqui voce podera encontrar informações sobre pessoas que exercem cargos públicos, gastos governamentais, além de diversas métricas de âmbito social, econômico, tecnológico e demográfico. :)")
 ])
